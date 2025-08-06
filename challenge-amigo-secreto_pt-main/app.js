0b0ee7225d73cd8e6cd209753a1b43dc61f7d8d0
Raul Douglas <rauldouglasoliveira@gmail.com>
2025-08-06 13:44:18 -0300

    Desenvolvendo o projeto

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,13 @@
+let listaDeAmigos = [];
+
+function adicionarAmigos() {
+    let nomeDigitado = document.getElementById('amigo');
+    let nome = nomeDigitado.value.trim();
+    if (nome === '') {
+        alert('Por favor, preencha com um nome válido.');
+        return;
+    }
+
+}
+
+
