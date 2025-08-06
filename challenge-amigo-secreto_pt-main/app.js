6c4241109e65c87da22a49a0100da6054efb20b6
Raul Douglas <rauldouglasoliveira@gmail.com>
2025-08-06 17:09:13 -0300

    Adicionando atualização de listas do projeto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -3,11 +3,24 @@ let listaDeAmigos = [];
 function adicionarAmigos() {
     let nomeDigitado = document.getElementById('amigo');
     let nome = nomeDigitado.value.trim();
+
     if (nome === '') {
-        alert('Por favor, preencha com um nome válido.');
+        alert('Por favor, digite um nome antes de adicionar.');
         return;
     }
 
+    listaDeAmigos.push(nome);
+    atualizarLista();
+    nomeDigitado.value = '';
 }
 
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
 
+    listaDeAmigos.forEach((amigo) => {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
+}
