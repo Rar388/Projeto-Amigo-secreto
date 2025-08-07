638cc81a063950782accc99e87d967a8f159fa3a
Raul Douglas <rauldouglasoliveira@gmail.com>
2025-08-07 12:19:27 -0300

    Modificando e adicionado recursos ao projeto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,26 +1,34 @@
+let texto = document.querySelector('h1')
+texto.innerHTML = 'Bem Vindo ao nosso Amigo Secreto!'
+let tag = document.querySelector('h2')
+tag.innerHTML = 'Digite o nome dos seus amigos abaixo';
 let listaDeAmigos = [];
 
-function adicionarAmigos() {
-    let nomeDigitado = document.getElementById('amigo');
-    let nome = nomeDigitado.value.trim();
 
-    if (nome === '') {
-        alert('Por favor, digite um nome antes de adicionar.');
-        return;
-    }
+function adicionarAmigos() { 
+  let input = document.querySelector('input'); 
+  let amigo = input.value
 
-    listaDeAmigos.push(nome);
-    atualizarLista();
-    nomeDigitado.value = '';
+  if (amigo === '') { 
+    alert('Por favor, digite um nome antes de adicionar.'); 
+  } else { 
+    listaDeAmigos.push(amigo);
+    listaAmigos();
+    limparCampo();
+  }
 }
 
-function atualizarLista() {
-    let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = '';
+function listaAmigos() { 
+  let ul = document.getElementById('listaAmigos'); 
+  ul.innerHTML = '';
 
-    listaDeAmigos.forEach((amigo) => {
-        let item = document.createElement('li');
-        item.textContent = amigo;
-        lista.appendChild(item);
-    });
+  for (let nome of listaDeAmigos) { 
+    let li = document.createElement('li');
+    li.textContent = nome;
+    ul.appendChild(li); 
+  }
 }
+
+
+
+
