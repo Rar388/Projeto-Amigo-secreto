35554d4c7199423de23b8c8c8d129e8281186d7c
Raul Douglas <rauldouglasoliveira@gmail.com>
2025-08-09 17:36:40 -0300

    Resultado final do projeto

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,8 +1,9 @@
 let texto = document.querySelector('h1')
-texto.innerHTML = 'Bem Vindo ao nosso Amigo Secreto!'
-let tag = document.querySelector('h2')
-tag.innerHTML = 'Digite o nome dos seus amigos abaixo';
+   texto.innerHTML = 'Bem Vindo ao Amigo Secreto!'
+   let tag = document.querySelector('h2')
+   tag.innerHTML = 'Digite o nome dos seus amigos';
 let listaDeAmigos = [];
+
 
 function adicionarAmigos() { 
   let input = document.querySelector('input'); 
@@ -18,19 +19,36 @@ function adicionarAmigos() {
 }
 
 function listaAmigos() { 
-  let ul = document.getElementById('listaAmigos'); 
-  ul.innerHTML = '';
-
-  for (let nome of listaDeAmigos) { 
+   let ul = document.getElementById('listaAmigos'); 
+   ul.innerHTML = '';
+   let contador = 0;
+ while (contador < listaDeAmigos.length) {
     let li = document.createElement('li');
-    li.textContent = nome;
-    ul.appendChild(li); 
+    li.textContent = listaDeAmigos[contador];
+    ul.appendChild(li);
+    contador++;
   }
 }
+
+
+function sortearAmigo() { 
+  let ul = document.getElementById('resultado');
+  ul.innerHTML = ''; 
+
+  if (listaDeAmigos.length == 0) { 
+    alert('Por favor, insira nomes para sortear.'); 
+    return; 
+  }
+
+  let gerarNomeAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+  let nomeAleatorio = listaDeAmigos[gerarNomeAleatorio]; 
+
+  let li = document.createElement('li');
+  li.textContent = `🥳🎉 O nome sorteado é: ${nomeAleatorio}`;
+  ul.appendChild(li);
+}
+
 function limparCampo() { 
   let input = document.querySelector('input');
   input.value = '';
 }
-
-
-
